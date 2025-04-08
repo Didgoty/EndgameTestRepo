402efaebefb2e119ae3ea2fbf54ac90b9bae1837
Didgoty <143346593+Didgoty@users.noreply.github.com>
2025-04-08 14:08:02 -0400

    Update infinity-upgrades.js
    Porting to BE

--- src/core/secret-formula/infinity/infinity-upgrades.js
+++ src/core/secret-formula/infinity/infinity-upgrades.js
@@ -4,7 +4,8 @@ function dimInfinityMult() {
   return Currency.infinitiesTotal.value.times(0.2).plus(1);
 }
 function chargedDimInfinityMult() {
-  return 1 + Math.log10(Math.max(1, Currency.infinitiesTotal.value.pLog10())) * Math.sqrt(Ra.pets.teresa.level) / 150;
+  return Decimal.log10(Decimal.max(1, Currency.infinitiesTotal.value.pLog10()))
+    .mul(Math.sqrt(Ra.pets.teresa.level) / 150).add(1);
 }
 
 export const infinityUpgrades = {
@@ -12,13 +13,13 @@ export const infinityUpgrades = {
     id: "timeMult",
     cost: 1,
     description: "Antimatter Dimensions gain a multiplier based on time played",
-    effect: () => Math.pow(Time.totalTimePlayed.totalMinutes / 2, 0.15),
+    effect: () => Decimal.pow(Time.totalTimePlayed.totalMinutes.div(2), 0.15),
     formatEffect: value => formatX(value, 2, 2),
     charged: {
       description: "Antimatter Dimensions gain a power effect based on time played and Teresa level",
-      effect: () => 1 +
-        Math.log10(Math.log10(Time.totalTimePlayed.totalMilliseconds)) *
-        Math.pow(Ra.pets.teresa.level, 0.5) / 150,
+      effect: () =>
+        Decimal.log10(Decimal.log10(Time.totalTimePlayed.totalMilliseconds))
+          .times(Decimal.pow(Ra.pets.teresa.level, 0.5)).div(150).add(1),
       formatEffect: value => formatPow(value, 4, 4)
     }
   },
@@ -116,14 +117,14 @@ export const infinityUpgrades = {
     id: "timeMult2",
     cost: 3,
     description: "Antimatter Dimensions gain a multiplier based on time spent in current Infinity",
-    effect: () => Decimal.max(Math.pow(Time.thisInfinity.totalMinutes / 4, 0.25), 1),
+    effect: () => Decimal.max(Decimal.pow(Time.thisInfinity.totalMinutes.div(4), 0.25), 1),
     formatEffect: value => formatX(value, 2, 2),
     charged: {
       description:
         "Antimatter Dimensions gain a power effect based on time spent in current Infinity and Teresa level",
-      effect: () => 1 +
-        Math.log10(Math.log10(Time.thisInfinity.totalMilliseconds + 100)) *
-        Math.sqrt(Ra.pets.teresa.level) / 150,
+      effect: () =>
+        Decimal.log10(Decimal.log10(Time.thisInfinity.totalMilliseconds.add(100)))
+          .times(Math.sqrt(Ra.pets.teresa.level)).div(150).add(1),
       formatEffect: value => formatPow(value, 4, 4)
     }
   },
@@ -163,15 +164,15 @@ export const infinityUpgrades = {
     formatEffect: value => {
       if (Teresa.isRunning || V.isRunning) return "Disabled in this reality";
       if (Pelle.isDoomed) return "Disabled";
-      if (player.records.bestInfinity.time >= 999999999999) return "Too slow to generate";
-      return `${format(value, 2)} every ${Time.bestInfinity.times(10).toStringShort()}`;
+      if (player.records.bestInfinity.time.gte(DC.BEMAX.log10())) return "Too slow to generate";
+      return `${format(value, 2)} every ${Time.bestInfinity.times(DC.E1).toStringShort()}`;
     },
     charged: {
       description: () =>
         `Gain Reality Machines each real-time second proportional to amount gained on Reality,
         increasing with Teresa level`,
-      effect: () => Math.pow(Ra.pets.teresa.level, 2) *
-        Ra.unlocks.continuousTTBoost.effects.autoPrestige.effectOrDefault(1),
+      effect: () => Decimal.mul(Math.pow(Ra.pets.teresa.level, 2),
+        Ra.unlocks.continuousTTBoost.effects.autoPrestige.effectOrDefault(1)),
       formatEffect: value => formatX(value, 2, 1)
     }
   },
@@ -211,7 +212,7 @@ export const infinityUpgrades = {
       ? `Only while offline, gain ${formatPercents(0.5)} of your best IP/min without using Max All`
       : "This upgrade would give offline Infinity Point generation, but offline progress is currently disabled"),
     effect: () => (player.options.offlineProgress
-      ? player.records.thisEternity.bestIPMsWithoutMaxAll.times(TimeSpan.fromMinutes(1).totalMilliseconds / 2)
+      ? player.records.thisEternity.bestIPMsWithoutMaxAll.times(TimeSpan.fromMinutes(1).totalMilliseconds.div(2))
       : DC.D0),
     isDisabled: () => !player.options.offlineProgress,
     formatEffect: value => `${format(value, 2, 2)} IP/min`,
@@ -225,7 +226,7 @@ export const infinityUpgrades = {
     description: () => `Multiply Infinity Points from all sources by ${formatX(2)}`,
     // Normally the multiplier caps at e993k or so with 3300000 purchases, but if the cost is capped then we just give
     // an extra e7k to make the multiplier look nice
-    effect: () => (player.IPMultPurchases >= 3300000 ? DC.E1E6 : DC.D2.pow(player.IPMultPurchases)),
+    effect: () => (player.IPMultPurchases.gte(3300000) ? DC.E1E6 : DC.D2.pow(player.IPMultPurchases)),
     cap: () => Effarig.eternityCap ?? DC.E1E6,
     formatEffect: value => formatX(value, 2, 2),
   }
