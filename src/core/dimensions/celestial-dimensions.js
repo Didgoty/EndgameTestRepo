22080596f07b00f0b716fb80f73a09ea5a373624
Didgoty <143346593+Didgoty@users.noreply.github.com>
2025-07-27 19:33:20 -0400

    Create celestial-dimensions.js

---
+++ src/core/dimensions/celestial-dimensions.js
@@ -0,0 +1,14 @@
+import { DC } from "../constants";
+
+import { DimensionState } from "./dimension";
+
+export function celestialDimensionCommonMultiplier() {
+  let multiplier = DC.D1;
+
+  
+
+  multiplier = multiplier.timesEffectsOf(
+      //stuff idk
+    )
+}
+
